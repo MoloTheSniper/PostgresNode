98f4863e0a45cc38108f39104002843c04d4346f
MoloTheSniper <molokomagwai@MacBookAir.lan>
2025-11-03 18:05:34 +0200

    POST Query done

--- main.jsx
+++ main.jsx
@@ -1,10 +1,34 @@
 const {Client} = require('pg');
-
+const express = require('express');
+const app = express();
+app.use(express.json());
+//client is the pg stuff we installed for Postgress
 const con = new Client({
-    host:"localhost",
+    host:"localhost", //because its on our pc we us localhost
     user: "postgres",
     port: 5432,
     password:"Laptop@hs",
     database:"demopost"
 })
 con.connect().then(()=>console.log("Connected"))
+
+//-------------Part2--------------
+app.post('/postData',(req,res)=>{
+
+    const {name, id} = req.body;
+    const insert_query = 'INSERT INTO demotable(name, id) VALUES($1,$2)';
+    con.query(insert_query,[name,id],(err,result) =>{
+        if(err)
+        {
+            res.send(err);
+        }
+        else{
+            console.log(result);
+            res.send("Posted Data!")
+        }
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running...");
+})
